920c5a2c83776cadc44a3ac00dcd40750e3d12e0
cgpllx <cgpllx1@qq.com>
2014-10-12 22:12:10 +0800

    Change-Id: I8b50c63e6ac0acdca5e4482e610302de72131632 Signed-off-by: cgpllx <cgpllx1@qq.com>

---
+++ src/com/kubeiwu/commontool/setting/view/GroupView.java
@@ -0,0 +1,50 @@
+package com.kubeiwu.commontool.setting.view;
+
+import java.util.ArrayList;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+import android.widget.LinearLayout;
+
+public class GroupView extends LinearLayout {
+
+	public GroupView(Context context, AttributeSet attrs, int defStyleAttr) {
+		super(context, attrs, defStyleAttr);
+		initGroupView();
+	}
+
+	public GroupView(Context context, AttributeSet attrs) {
+		this(context, attrs, 0);
+	}
+
+	public GroupView(Context context) {
+		super(context);
+		initGroupView();
+	}
+
+	private void initGroupView() {
+		setOrientation(VERTICAL);
+	} 
+
+	private ArrayList<RowDescriptor> descriptors = new ArrayList<>();
+
+ 
+	public void addAllRowView(ArrayList<RowDescriptor> descriptors) {
+		this.descriptors.addAll(descriptors);
+	}
+
+	public void notifyDataChanged() {
+		if (descriptors != null && descriptors.size() > 0) {
+			RowView rowView;
+			for (RowDescriptor descriptor : descriptors) {
+				rowView=new RowView(getContext());
+				rowView.initRowViewData(descriptor);
+				rowView.notifyDataChanged();
+				addView(rowView);
+			}
+		} else {
+			setVisibility(View.GONE);
+		}
+	}
+}
